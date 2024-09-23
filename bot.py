698ef5d059d2a31eb4996db9aa1cde63b7cac6e4
Ios <ioskanadaserver0@gmail.com>
2024-09-23 21:57:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,25 @@ import sys
 
 from src.core import main
 from src.deeplchain import _banner, _clear, log,hju,pth,mrh
+from keep_alive import keep_alive
+import websockets
+from loguru import logger
+from flask import Flask
+# Flask application
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+def run_flask():
+    app.run(debug=True)
+
+init(autoreset=True)
 
 if __name__ == "__main__":
     _clear()
+    keep_alive()
     _banner()
     log(hju + f"Please wait, starting your bot...")
     log(pth + "~" * 38)
